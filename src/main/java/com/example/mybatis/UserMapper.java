c049afc799e920651384bf0396a8bcc77102462c
xiexiaoxuan <xiexiaoxuan1998@gmail.com>
2022-06-08 22:19:28 +0800

    Initial commit

---
+++ src/main/java/com/example/mybatis/UserMapper.java
@@ -0,0 +1,8 @@
+package com.example.mybatis;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.example.mybatis.pojo.User;
+
+public interface UserMapper extends BaseMapper<User> {
+
+}
---
+++ src/main/java/com/example/mybatis/pojo/User.java
@@ -0,0 +1,15 @@
+package com.example.mybatis.pojo;
+
+import lombok.*;
+
+@Data
+public class User {
+
+    private Long id;
+
+    private String name;
+
+    private Integer age;
+
+    private String email;
+}
